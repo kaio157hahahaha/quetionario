7e04a96d0544bd4097185cb5f71d20e07009eea7
kaio157hahahaha <148557177+kaio157hahahaha@users.noreply.github.com>
2024-08-27 11:07:52 -0300

    hahahahaha

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-const caixaPricipal 
+const caixaPricipal = document.querySelector("caixa-principal");
+const caixaPricipal = document.querySelector("caixa-parguntas");
+const caixaPricipal = document.querySelector("caixa-alternativas");
+const caixaPricipal = document.querySelector("caixa-resultado");
+const caixaPricipal = document.querySelector("texto-resultado") ;   
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    },
+    {
+        enunciado: "Pergunta 4",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    },
+    
+
+]
